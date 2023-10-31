4c90a35321d0426fe265536aefb53e01b5514126
Charusheel <103216731+Wow9438@users.noreply.github.com>
2023-10-31 22:58:44 +0530

    Update grun.sh

--- parser/grun.sh
+++ parser/grun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-lex lexer_for_parse.l
+lex lexer_for_parser.l
 bison -t -d -Wcounterexample parser.y
 gcc lex.yy.c parser.tab.c
 ./a.out input.txt #input file name should be changed as per the need
